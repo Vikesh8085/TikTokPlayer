ec7324eae3374978dd706c28093f6f869def64c4
vikesh <vikesh.prasad@mobiotics.com>
2020-05-29 07:15:34 +0530

    Initial Commit

---
+++ TikTokPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TikTokPlayer
+//
+//  Created by Vikesh Prasad on 29/05/20.
+//  Copyright © 2020 Learning. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
